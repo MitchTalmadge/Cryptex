8faac25879795db94bb0d7f5cdf12683af3a2ebe
Mitch Talmadge <mitch@mitchtalmadge.com>
2018-01-11 20:39:53 -0700

    Add basic backend framework

---
+++ src/backend/main/java/com/mitchtalmadge/cryptex/CryptexApplication.java
@@ -0,0 +1,16 @@
+package com.mitchtalmadge.cryptex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+public class CryptexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptexApplication.class, args);
+    }
+}
---
+++ src/backend/main/java/com/mitchtalmadge/cryptex/web/api/controllers/ExampleController.java
@@ -0,0 +1,10 @@
+package com.mitchtalmadge.cryptex.web.api.controllers;
+
+import com.mitchtalmadge.cryptex.web.api.APIControllerAbstract;
+import com.mitchtalmadge.cryptex.web.api.annotations.APIController;
+
+
+@APIController
+public class ExampleController extends APIControllerAbstract {
+
+}
